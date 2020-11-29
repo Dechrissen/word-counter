16d03ceb563304d768b494fb059f0519a8636fd8
Derek Andersen <41839810+Dechrissen@users.noreply.github.com>
2020-11-28 19:01:13 -0500

    Initial commit

---
+++ Count.py
@@ -0,0 +1,33 @@
+import re
+import codecs
+import nltk
+from nltk.corpus import stopwords
+from collections import Counter
+
+def remove_punctuation(text):
+    return re.sub(r'[^\w\s]','', text).lower()
+
+word_frequencies = {}
+corpus = ''
+
+filename = 'corpus.txt'
+with codecs.open(filename, 'r', encoding='utf8') as file:
+    corpus = file.read()
+
+stop_words = set(stopwords.words('english'))
+
+corpus = remove_punctuation(corpus)
+corpus = corpus.split()
+filtered_corpus = [word for word in corpus if word not in stop_words]
+
+counts = dict(Counter(filtered_corpus))
+
+counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
+
+limit = 20
+start = 0
+for item in counts.keys():
+    print(item + ':', counts[item])
+    start += 1
+    if start >= limit:
+        break
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# word-counts
- 
+# Document Word Counter
+
+## Requirements
+
+- Python 3
+- `nltk` package
+
+## Usage
+
+- `cd` to this directory in your terminal
+- Run `pip install -r requirements.txt` or `pip3 install -r requirements.txt`
+- Paste the document to be tokenized and counted into `corpus.txt`
+- Run `python Count.py` or `python3 Count.py`
